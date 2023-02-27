bdd99b71bda43f4449dd8d34dc68b62dee432485
Haresh1204 <chauhanharesh522@gmail.com>
2023-02-27 21:40:59 +0530

    first commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Title from "../Component/Title";
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+
+        <Title
+          title="title"
+          description="India, officially the Republic of India (Hindi: Bhārat Gaṇarājya),[25] is a country in South Asia. It is the seventh-largest country by area, the second-most populous country, and the most populous democracy in the world. Bounded by the Indian Ocean on the south, the Arabian Sea on the southwest, and the Bay of Bengal on the southeast, it shares land borders with Pakistan to the west;"
+        />
+      </>
+    );
+  }
+}
+
+export default Home;
